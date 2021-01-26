c5e7009fd114c7f296a3d47c7f4e395561c647be
Raghav Shrivastav <raghavbpdshrivastav@gmail.com>
2021-01-26 14:57:08 -0500

    components: add sidedrawer component and create portal to ensure it renders outside the root

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -3,24 +3,32 @@ import { Link } from "react-router-dom";
 
 import Header from "./Header";
 import NavLinks from "./NavLinks";
+import SideDrawer from "./SideDrawer";
 
 import "./MainNavigation.css";
 
 const MainNavigation = (props) => {
   return (
-    <Header>
-      <button className="main-navigation__menu-btn">
-        <span />
-        <span />
-        <span />
-      </button>
-      <h1 className="main-navigation__title">
-        <Link to="/">Your Holidays</Link>
-      </h1>
-      <nav>
-        <NavLinks />
-      </nav>
-    </Header>
+    <React.Fragment>
+      <SideDrawer>
+        <nav className="main-navigation__drawer-nav">
+          <NavLinks />
+        </nav>
+      </SideDrawer>
+      <Header>
+        <button className="main-navigation__menu-btn">
+          <span />
+          <span />
+          <span />
+        </button>
+        <h1 className="main-navigation__title">
+          <Link to="/">Your Holidays</Link>
+        </h1>
+        <nav className="main-navigation__header-nav">
+          <NavLinks />
+        </nav>
+      </Header>
+    </React.Fragment>
   );
 };
 
