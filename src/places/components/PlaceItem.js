4e3ffd0e72bc0049e8d2d2b132ede34a13beac56
Raghav Shrivastav <raghavbpdshrivastav@gmail.com>
2021-01-28 16:22:12 -0500

    components: render button on place item

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Card from "../../shared/components/UIElements/Card";
+import Button from "../../shared/components/FormElements/Button";
 
 import "./PlaceItem.css";
 
@@ -16,9 +17,9 @@ const PlaceItem = (props) => {
           <p>{props.description}</p>
         </div>
         <div className="place-item__actions">
-          <button>View on Map</button>
-          <button>Edit</button>
-          <button>Delete</button>
+          <Button>View on Map</Button>
+          <Button>Edit</Button>
+          <Button>Delete</Button>
         </div>
       </Card>
     </li>
