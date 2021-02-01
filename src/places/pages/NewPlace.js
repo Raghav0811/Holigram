53c56a2118c3e2551e7eb00bdc96777d009b483b
Raghav Shrivastav <raghavbpdshrivastav@gmail.com>
2021-02-01 13:18:35 -0500

    components: complete new place form template and add modular input form to shared folder

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,7 +1,15 @@
 import React from "react";
 
+import "../../shared/components/FormElements/Input";
+import Input from "../../shared/components/FormElements/Input";
+import "./NewPlace.css";
+
 const NewPlace = () => {
-  return <h2>New place works</h2>;
+  return (
+    <form className="place-form">
+      <Input type="text" label="title" />
+    </form>
+  );
 };
 
 export default NewPlace;
