c9c34161636cfbb59873e24eff440066e717bb41
Raghav Shrivastav <raghavbpdshrivastav@gmail.com>
2021-02-03 10:11:42 -0500

    component: start work on the update place component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {
 } from "react-router-dom";
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
+import UpdatePlace from "./places/pages/UpdatePlace";
 import UserPlaces from "./places/pages/UserPlaces";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
@@ -23,6 +24,9 @@ const App = () => {
         </Route>
         <Route path="/places/new" exact>
           <NewPlace />
+        </Route>
+        <Route path="/places/:placeId">
+          <UpdatePlace />
         </Route>
       </Switch>
       <Redirect to="/" />
